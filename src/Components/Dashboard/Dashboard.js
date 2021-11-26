2b40fea65acf5ce5ef07496957e93ca47282d5a6
samimeadad <sami.meadad@gmail.com>
2021-11-26 19:31:48 +0600

    dashboard body component created

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import DashboardBody from '../DashboardBody/DashboardBody';
 import Header from '../Header/Header';
 
 const Dashboard = () => {
     return (
         <div>
             <Header></Header>
+            <DashboardBody></DashboardBody>
         </div>
     );
 };
---
+++ src/Components/DashboardBody/DashboardBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const DashboardBody = () => {
+    return (
+        <Container className="mt-5 text-start">
+            <h2>Dashboard</h2>
+        </Container>
+    );
+};
+
+export default DashboardBody;
